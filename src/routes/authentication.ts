26c948d10d247f36ae9eb6b4172185165b7d4a5f
harugitsune <harugitsune27@icloud.com>
2024-10-17 15:14:18 +0900

    [Add]  auth

---
+++ src/routes/authentication.ts
@@ -0,0 +1,44 @@
+import { Request, Response, NextFunction } from "express";
+import { authentication } from "../common/Encryption";
+import { exist } from "../common/Validation";
+import { prisma } from "../server";
+
+// ルーティングモジュールを呼び出し
+const router = require("express").Router();
+
+router.post("/", async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        exist(req.body.email, req.body.password);
+
+        // ハッシュ化パスワードとアカウントの有効性を取得
+        const accountData = (await prisma.authentications.findFirst({
+            where: {
+                email: req.body.email
+            }
+        }));
+
+        if (accountData == null) {
+            throw new Error("Can't find account data");
+        } else if (!accountData.active) {
+            throw new Error("Account is not active");
+        } else if (accountData.password) {
+            // エラー処理等
+            if (await authentication(req.body.password, accountData.password)) {
+                // パスワードが正しかった場合セッションにユーザデータを登録
+                req.session.userId = accountData.id;
+                res.json({message: "Authentication was successful"});
+            } else {
+                // パスワードが間違っていた場合
+                throw new Error("Password is incorrect");
+            }
+        } else {
+            // それ以外のエラー
+            throw new Error("Something wrong");
+        }
+
+    } catch(e) {
+        next(e);
+    }
+});
+
+module.exports = router;
--- src/server.ts
+++ src/server.ts
@@ -1,26 +1,67 @@
 import { PrismaClient } from "@prisma/client";
 import express, { Request, Response, NextFunction } from "express";
+import helmet from "helmet";
+import cors from "cors";
+import expressSession from "express-session";
+import { PrismaSessionStore } from "@quixo3/prisma-session-store";
+
+declare module 'express-session' {
+    export interface SessionData {
+        userId: number;
+    }
+}
 
 const app = express();
 export const prisma = new PrismaClient();
 
 // ポート番号の指定
 const port = 6666;
- 
+
+// cors許可設定
+app.use(cors());
+
+// 簡単なセキュリティ対策
+app.use(helmet());
+
+// セッション管理
+app.use(
+    expressSession({
+        name: "sotsuken.sid",
+        cookie: {
+            maxAge: 1000 * 60 * 60
+        },
+        secret: "secret",
+        resave: true,
+        saveUninitialized: true,
+        store: new PrismaSessionStore(
+               new PrismaClient(),
+               {
+                    checkPeriod: 2 * 60 * 1000,
+                    dbRecordIdIsSessionId: true,
+                    dbRecordIdFunction: undefined,
+               } 
+            )
+        }
+    )
+);
 
 // フォームデータの解析
 app.use(express.urlencoded({ extended: false }));
-app.use(express.json()); // JSONデータのパースも許可
 
+// JSONデータのパースも許可
+app.use(express.json());
+
+// アクセスログ出力
 app.use((req: Request, res: Response, next: NextFunction) => {
-    // ログ出力
     console.log(`Request recieved from ${req.ip} to ${req.path}`);
     next();
 });
 
 // ルーティング
+app.use("/registration", require("./routes/registration"))
+app.use("/authentication", require("./routes/authentication"))
 app.use("/user", require("./routes/user"));
-app.use("/company", require("./routes/company"))
+app.use("/company", require("./routes/company"));
 
 // エラー処理
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
