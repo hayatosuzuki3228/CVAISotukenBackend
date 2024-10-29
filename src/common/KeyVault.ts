eab461a4671e06f956eb0bcdc162e2159ed12bb2
harugitsune <harugitsune27@icloud.com>
2024-10-29 14:23:58 +0900

    fix

---
+++ src/common/KeyVault.ts
@@ -0,0 +1,22 @@
+const { SecretClient } = require("@azure/keyvault-secrets");
+const { DefaultAzureCredential } = require("@azure/identity");
+
+// Build the URL to reach your key vault
+const vaultName = "nkc-labo24-aix23-kv"; //Key Vaultのリソース名
+const url = `https://${vaultName}.vault.azure.net`;
+
+let credential: any = null;
+
+// Lastly, create our keys client and connect to the service
+if( process.platform !== "win32" ) credential = new DefaultAzureCredential();
+
+async function getPepper() {
+    if (credential) {
+        const secretClient = new SecretClient(url, credential); //シークレット取得のためのclient
+        const secretResult = await secretClient.getSecret("pepper"); //シークレットの取得結果
+        return secretResult.value;
+    }
+    return null
+}
+
+export const pepper = getPepper();
