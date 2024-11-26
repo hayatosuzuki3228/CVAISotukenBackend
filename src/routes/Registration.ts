85bfca00d822bbab49eeafa067651fe3cbb0f6f9
Kaketo Takama <nkc20232472@st.denpa.jp>
2024-11-26 12:37:19 +0900

    [add] work_location add

--- src/routes/Registration.ts
+++ src/routes/Registration.ts
@@ -77,7 +77,7 @@ router.post("/student/qulification", async (req: Request, res: Response, next: N
 router.post("/student/status", async (req: Request, res: Response, next: NextFunction) => {
     try {
         exist(req.body.email, req.body.password);
-        exist(req.body.name, req.body.furigana, req.body.gender, req.body.birthday, req.body.residence, req.body.graduation_year, req.body.classId, req.body.work_location);
+        exist(req.body.name, req.body.furigana, req.body.gender, req.body.birthday, req.body.residence, req.body.graduation_year, req.body.classId);
 
         if (await prisma.student.findFirst({
             where: {
@@ -102,8 +102,7 @@ router.post("/student/status", async (req: Request, res: Response, next: NextFun
                             birthday: new Date(req.body.birthday),
                             residence: req.body.residence,
                             graduation_year: Number(req.body.graduation_year),
-                            classId: req.body.classId,
-                            work_location: req.body.work_location
+                            classId: req.body.classId
                         },
                     },
                 },
@@ -131,7 +130,7 @@ router.post("/student/status", async (req: Request, res: Response, next: NextFun
 router.post("/student/all", async (req: Request, res: Response, next: NextFunction) => {
     try {
         exist(req.body.email, req.body.password, req.body.qualificationId);
-        exist(req.body.name, req.body.furigana, req.body.gender, req.body.birthday, req.body.residence, req.body.graduation_year, req.body.classId, req.body.work_location);
+        exist(req.body.name, req.body.furigana, req.body.gender, req.body.birthday, req.body.residence, req.body.graduation_year, req.body.classId);
 
         const data: Prisma.StudentQualificationCreateManyStudentInputEnvelope = {
             data: req.body.qualificationId.map((id: any) => ({
@@ -165,8 +164,7 @@ router.post("/student/all", async (req: Request, res: Response, next: NextFuncti
                             birthday: new Date(req.body.birthday),
                             residence: req.body.residence,
                             graduation_year: Number(req.body.graduation_year),
-                            classId: req.body.classId,
-                            work_location: req.body.work_location
+                            classId: req.body.classId
                         },
                     },
                 },
